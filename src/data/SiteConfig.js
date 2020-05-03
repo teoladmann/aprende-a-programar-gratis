46b0fe1472bbd4c607548fdf78eac1c3dc9456e6
Timoteo Ladmann <teoladmann@gmail.com>
2020-05-03 15:19:02 -0300

    add site config object

---
+++ src/data/SiteConfig.js
@@ -0,0 +1,9 @@
+const config = {
+  siteTitle: 'Aprende a Programar Gratis',
+  siteTitleShort: 'Aprende Programación',
+  siteTitleAlt: 'Aprende a Programar Gratis',
+  siteUrl: 'https://www.aprendeaprogramargratis.com',
+  siteDescription: 'Aprende a programar de forma gratuita sin salir de tu casa.'
+}
+
+module.exports = config;
