5233b6c0bc1e9e7ebd6bafb80741365a8b6456aa
Timoteo Ladmann <teoladmann@gmail.com>
2020-05-03 15:17:47 -0300

    add navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const Navigation = () => {
+  return (
+    <nav className="nav-container">
+      <div className="brand">
+        <Link to="/">
+          <span className="text"><span className="emoji" role="img" aria-label="Computadora">💻</span>Aprende a Programar Gratis</span>
+        </Link>
+      </div>
+      <div className="links">
+        <Link to="/acerca">
+          <span>
+            Acerca
+          </span>
+        </Link>
+      </div>
+   </nav>
+  )
+}
+
+export default Navigation;
